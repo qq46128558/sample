bcd161cbbdc3749c6828eb0affaf3850accc1c28
peter <46128558@qq.com>
2017-06-23 14:29:28 +0800

    update bbc api sample

--- bbc/bbc_api_trade_shop_get.php
+++ bbc/bbc_api_trade_shop_get.php
@@ -1,5 +1,5 @@
 <?php
-/*获取商品详情,无Oauth认证*/
+/*(商家)获取单笔交易信息, 需要Oauth认证, 未实现*/
 error_reporting(E_ALL & ~E_NOTICE);
 
 //Unicode解码函数
@@ -43,19 +43,60 @@ function curl($url,$post_data){
 }
 
 $params = array(
-	'method'=>'item.detail',
+  
+	'method'=>'trade.shop.get',
 	'format'=>'json', //返回数据是json格式的，目前只支持json
 	'v'=>'v1', //标识该接口的版本
-  //'timestamp'=>time(),
-  //'sign_type'=>'MD5',
-	'item_id'=>28, //商品id。必须是正整数
-	// 'fields'=>'', //(暂未知如何传入)要获取的商品字段集。多个字段用“,”分隔
+	'shop_id'=>3,
+  'tid'=>'1601261431290002',
+  'fields'=>'*',
+  'timestamp'=>time(),
+  'sign_method'=>'md5',
+  'sign_type'=>"MD5", 
+  // 'oid'=>'',
+
+  /*
+  'client_id'=>'nk2l73rt',
+  // 'client_id'=>'j22imnvy',
+  'ApiId'=>'qwddqhx2',
+  'RequestId'=>'4dlzcqpdfbb6jkor',
+  'ApiMethod'=>'trade.shop.get',
+  'Key'=>'nk2l73rt',
+  */
+
+  /*
+  'method'=>'open.oauth.login',
+  'format'=>'json',
+  'v'=>'v1',
+  'loginname'=>'onexbbc',
+  'password'=>'demo123',
+  'timestamp'=>time(),
+  'sign_type'=>'MD5',
+  'oauth_type'=>'seller',
+  */
+  
 );
+
+// $token='2ed6c12c7b8a1a2c84b064ebe47263152c0b655355470714862bcc8dbdd949b2';
 $token='';
 $params['sign'] = strtoupper(md5(strtoupper(md5(assemble($params))).$token));
-//此处结尾是topapi
-$url = "192.168.239.138/index.php/topapi";
+//echo $params['sign'];
+//此处结尾是api
+$url = "192.168.239.138/index.php/api";
+// $url="192.168.239.138:8080/api/systrade/"
 $output = curl($url,$params);
 echo unicode_decode($output);
 
+
+/*资料记录
+app/topdev/controller/apis.php
+app/topdev/view/apis/test.html
+
+app/systrade/api/getTradeInfoByShop.php
+config/apis.php
+
+app/system/middleware/checkApiSystemParams.php
+
+app/base/lib/rpc/validate.php
+*/
 
