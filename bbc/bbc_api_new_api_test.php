a48d856fdf1cf630ea0515de5cb7aa5fc679b018
peter <46128558@qq.com>
2017-08-08 15:52:31 +0800

    update api unicode function

--- bbc/bbc_api_new_api_test.php
+++ bbc/bbc_api_new_api_test.php
@@ -9,7 +9,14 @@ function replace_unicode_escape_sequence($match) {
 function unicode_decode($str) {
  return preg_replace_callback('/\\\u([0-9a-f]{4})/i', 'replace_unicode_escape_sequence', $str); 
 }
-
+/*在类中的写法
+function unicode_decode($str) { 
+  return preg_replace_callback('/\\\u([0-9a-f]{4})/i', 
+    function($match){
+      return mb_convert_encoding(pack('H*', $match[1]), 'UTF-8', 'UCS-2BE');
+    }, $str);  
+}
+*/
 
 function assemble($params){
   if(!is_array($params)){
