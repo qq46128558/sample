bd09d274e2ab4de3fbec3cbcecbda881ebb31338
peter <46128558@qq.com>
2017-09-05 20:55:06 +0800

    add node course: fs

---
+++ node/course/module/fs.js
@@ -0,0 +1,51 @@
+'use strict';
+// 异步读文件
+var fs=require('fs');
+fs.readFile('hello.js','utf-8',function(err,data){
+    if (err){
+        console.log(err);
+    }else{
+        console.log('01.');
+        console.log(data);
+        // string->buffer
+        console.log('02.');
+        console.log(Buffer.from(data,'utf-8'))
+    }
+})
+
+// 读取二进制文件
+fs.readFile('d:/projects/www/sample/1353022505371.jpg',function(err,data){
+    if (err){
+        console.log(err);
+    }else{
+        console.log('03.');
+        console.log(data);
+        console.log(data.length+' Bytes');
+        // buffer->string
+        console.log('04.太大会卡一段时间,略.');
+        // console.log(data.toString('utf-8'));
+    }
+})
+
+// 写文件writeFile,追加用appendFile(无换行)
+var now=new Date();
+fs.writeFile('output.txt','写资料入文件:'+now.getTime(),function(err){
+    if (err){
+        console.log(err);
+    }else{
+        console.log('05.write file done.');
+    }
+})
+
+// 获取文件或目录的详细信息
+fs.stat('hello.js',function(err,stats){
+    if (err){
+        console.log(err);
+    }else{
+        console.log('06.大小'+stats.size+ " Bytes");
+        console.log('07.是否文件'+stats.isFile());
+        console.log('08.是否目录'+stats.isDirectory());
+        console.log('09.创建时间'+stats.birthtime);
+        console.log('10.修改时间'+stats.mtime);
+    }
+})
