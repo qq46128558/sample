f78ce659fdc77d162baec3e91bea19483c6d6d6c
peter <46128558@qq.com>
2017-09-17 16:07:08 +0800

    add node course mini-excel

--- node/course/mvvm/hello-vue/app.js
+++ node/course/mvvm/hello-vue/app.js
@@ -1,12 +1,32 @@
-const Koa=require("koa");
-const app=new Koa();
-const staticFiles=require('./static-files');
+const Koa = require("koa");
+const app = new Koa();
+const staticFiles = require('./static-files');
 
-app.use(async(ctx,next)=>{
+const bodyParser = require('koa-bodyparser');
+const controller = require('./controller');
+const rest = require('./rest');
+const isProduction = process.env.NODE_ENV === 'production';
+
+
+app.use(async (ctx, next) => {
     console.log(`Process ${ctx.request.method} ${ctx.request.url}...`);
+    var
+        start = new Date().getTime(),
+        execTime;
     await next();
+    execTime = new Date().getTime() - start;
+    ctx.response.set('X-Response-Time', `${execTime}ms`);
 })
-app.use(staticFiles('/static',__dirname+'/static/'));
+app.use(staticFiles('/static', __dirname + '/static/'));
+
+// parse request body:
+app.use(bodyParser());
+
+// bind .rest() for ctx:
+app.use(rest.restify());
+
+// add controllers:
+app.use(controller());
 
 app.listen(3000);
 console.log('app started at port 3000...');
