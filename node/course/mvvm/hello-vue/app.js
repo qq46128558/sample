6c6a720e22d164a6cedbe4dbf7a13a7d1507c7ec
peter <46128558@qq.com>
2017-09-17 14:06:20 +0800

    update hello-vue form-vue

--- node/course/mvvm/hello-vue/app.js
+++ node/course/mvvm/hello-vue/app.js
@@ -1,5 +1,12 @@
 const Koa=require("koa");
 const app=new Koa();
+const staticFiles=require('./static-files');
+
+app.use(async(ctx,next)=>{
+    console.log(`Process ${ctx.request.method} ${ctx.request.url}...`);
+    await next();
+})
+app.use(staticFiles('/static',__dirname+'/static/'));
 
 app.listen(3000);
 console.log('app started at port 3000...');
