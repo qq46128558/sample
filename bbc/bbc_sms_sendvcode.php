f980ceb3502d0c457eac9627646e82f4fbee1031
peter <dgw@yn-ce.com>
2017-12-12 10:25:42 +0800

    update bbc sms send

--- bbc/bbc_sms_sendvcode.php
+++ bbc/bbc_sms_sendvcode.php
@@ -5,12 +5,21 @@ require_once __DIR__.'/../../b2b2c/bootstrap/start.php';
 /*发送短信验证码*/
 echo kernel::single('topc_passport')->sendVcode('13928016056','signup');
 
+// app/topm/lib/passport.php (或 app/topc/lib/passport.php) 的 sendVcode()方法
+// app/system/lib/data/user/vcode.php, send_sms()方法
+// app/system/lib/messenger.php 
+//     send($sendTo,$title,$contents,$config);
+//     sendSms($mobile,$tmpl,$data);    
+
 /*sendType类型
-activation
-reset
-forgot
-unreset
+activation 激活(身份验证?)
+reset 重置手机号或者邮箱
+forgot 找回密码
+unreset 重置手机号或者邮箱
 signup //手机注册
+depost_forgot //支付密码忘记密码
+auth_shop //商家安全验证
+findPw_shop //商家找回密码
 depost_forgot //支付密码忘记密码
 */
 
