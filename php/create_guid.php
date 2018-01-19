90300588e42e871270f5a439fd24bfacdc4694ad
peter <dgw@yn-ce.com>
2018-01-19 14:54:15 +0800

    update php guid

--- php/create_guid.php
+++ php/create_guid.php
@@ -1,5 +1,17 @@
 <?php
 
+/* 生成像GUID的随机数
+int mt_rand  ( int $min  , int $max  )
+返回 0 到 mt_getrandmax() 2147483647 之间的伪随机数。例如想要 5 到 15（包括 5 和 15）之间的随机数，用 mt_rand(5, 15)。
+
+string uniqid  ([ string $prefix  = ""  [, bool $more_entropy  = false  ]] )
+获取一个带前缀、基于当前时间微秒数的唯一ID。 
+prefix为空，则返回的字符串长度为13。more_entropy 为 TRUE ，则返回的字符串长度为23。 
+
+string md5  ( string $str  [, bool $raw_output  = false  ] )
+计算字符串的 MD5 散列值 
+以 32 字符十六进制数字形式返回散列值。 
+*/
 function create_guid(){
     $charid=strtoupper(md5(uniqid(mt_rand(), true)));
     // echo $charid;
