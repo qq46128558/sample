cd7e7607f0dc5430a389671d29f1156e861e1b49
peter <dgw@yn-ce.com>
2018-02-05 10:55:17 +0800

    scraping content decode

---
+++ python-scraping/06读取文档/content_decode.py
@@ -0,0 +1,13 @@
+from urllib.request import urlopen
+from bs4 import BeautifulSoup
+
+html=urlopen("http://en.wikipedia.org/wiki/Python_(programming_language)")
+bsObj=BeautifulSoup(html,'html.parser')
+content=bsObj.find("div",{"id":"mw-content-text"}).get_text()
+content=bytes(content,"UTF-8")
+content=content.decode("UTF-8")
+print(content)
+
+# 网站编码方式
+# charset=bsObj.find("meta").attrs["charset"]
+# print(charset)
