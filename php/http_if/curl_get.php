646e404ac46b60ffa9be57f566b65be2842a0d1e
peter <dgw@yn-ce.com>
2018-03-01 12:20:31 +0800

    php curl get

---
+++ php/http_if/curl_get.php
@@ -0,0 +1,26 @@
+<?php
+/* 
+GET请求 CURL
+php利用curl实现http get请求
+实现自定义http头
+*/
+
+//初始化
+$curl = curl_init();
+//设置抓取的url
+curl_setopt($curl, CURLOPT_URL, 'https://tmt.hoiyee.net/common/qiniu/token');
+//设置头文件的信息作为数据流输出
+curl_setopt($curl, CURLOPT_HEADER, 0);
+//设置获取的信息以文件流的形式返回，而不是直接输出。
+curl_setopt($curl, CURLOPT_RETURNTRANSFER, 1);
+// 设置自定义的HTTP头
+$headers = array();
+$headers[] = 'User-Agent:Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/63.0.3239.132 Safari/537.36';
+curl_setopt($curl, CURLOPT_HTTPHEADER, $headers);
+//执行命令
+$data = curl_exec($curl);
+//关闭URL请求
+curl_close($curl);
+//显示获得的数据
+var_dump($data);
+
