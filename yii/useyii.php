6fbdc56ce163cf43a1058da2baeb85a917e3c28d
peter <dgw@yn-ce.com>
2018-03-15 13:44:38 +0800

    Sample code improved: M yii/test.php  M yii/useyii.php  M yii/yii.md

--- yii/useyii.php
+++ yii/useyii.php
@@ -6,9 +6,15 @@
 defined('YII_DEBUG') or define('YII_DEBUG', true);
 defined('YII_ENV') or define('YII_ENV', 'dev');
 
-require(__DIR__ . '/../vendor/autoload.php');
-require(__DIR__ . '/../vendor/yiisoft/yii2/Yii.php');
+// require(__DIR__ . '/../vendor/autoload.php');
+// require(__DIR__ . '/../vendor/yiisoft/yii2/Yii.php');
+require(__DIR__ . '/../../yiibasic/vendor/autoload.php');
+require(__DIR__ . '/../../yiibasic/vendor/yiisoft/yii2/Yii.php');
 
-$config = require(__DIR__ . '/../config/web.php');
+// $config = require(__DIR__ . '/../config/web.php');
+$config = require(__DIR__ . '/../../yiibasic/config/web.php');
 
 (new yii\web\Application($config));
+
+//使用Yii类
+// 控制器中如果没有use Yii(namespace yii\web),则可以用\Yii调用
--- yii/yii.md
+++ yii/yii.md
@@ -1,3 +1,6 @@
 ##### 获取CSRF Token
     Yii::$app->request->getCsrfToken();
     Yii::$app->request->csrfToken;
+
+##### 获取版本
+    Yii::getVersion();
