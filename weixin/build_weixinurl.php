6c312ee4862bd2911c043c387f4a04fa6a71d1ed
peter <dgw@yn-ce.com>
2018-03-21 11:24:34 +0800

    Sample code improved: M weixin/build_weixinurl.php

--- weixin/build_weixinurl.php
+++ weixin/build_weixinurl.php
@@ -7,11 +7,12 @@ require_once('https_method.php');
 $redirecturl="http://ser.yn-ce.com/wx/webpage_auth.php";
 $redirecturl=urlencode($redirecturl);
 
-// state是自定义传参
-// scope=snsapi_base
-// $url="https://open.weixin.qq.com/connect/oauth2/authorize?appid=".$appinfo->t_appid."&redirect_uri=$redirecturl&response_type=code&scope=snsapi_base&state=0#wechat_redirect";
-
-// scope=snsapi_userinfo
-$url="https://open.weixin.qq.com/connect/oauth2/authorize?appid=".$appinfo->t_appid."&redirect_uri=$redirecturl&response_type=code&scope=snsapi_userinfo&state=1#wechat_redirect";
-
+if (isset($_GET['scope'])){
+    // state是自定义传参
+    echo 'scope=snsapi_base</br>';
+    $url="https://open.weixin.qq.com/connect/oauth2/authorize?appid=".$appinfo->t_appid."&redirect_uri=$redirecturl&response_type=code&scope=snsapi_base&state=0#wechat_redirect";
+}else{
+    echo 'scope=snsapi_userinfo</br>';
+    $url="https://open.weixin.qq.com/connect/oauth2/authorize?appid=".$appinfo->t_appid."&redirect_uri=$redirecturl&response_type=code&scope=snsapi_userinfo&state=1#wechat_redirect";
+}
 echo $url;
