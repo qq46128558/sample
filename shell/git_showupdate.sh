d3083ea9b03c1fb549b958727d2c29750e1d71db
peter <dgw@yn-ce.com>
2018-03-27 13:29:38 +0800

    Sample code improved: M shell/git_showupdate.sh

--- shell/git_showupdate.sh
+++ shell/git_showupdate.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
-# 取第一个参数值
+# 取第一个参数值,支持时间,如: "2018-03-27 09:00"
 after=$1
 # 如无值取当前日期
-if [ -z ${after} ]; then after=`date +%Y-%m-%d`; fi
+if [ -z "${after}" ]
+then 
+    after=`date +"%Y-%m-%d 00:00"`
+fi
 # 查看日期之后新增修改删除过的文件,结果存入临时文件
-git log --after=${after} --name-only --pretty=format:''|xargs -i echo {}>>temp.log
+git log --after="${after}" --name-only --pretty=format:''|xargs -i echo {}>>temp.log
 # 按行读取临时文件入files变量,为了可以for循环
 files=`xargs -a temp.log -n 1`
 # 置空临时文件,为了复用
