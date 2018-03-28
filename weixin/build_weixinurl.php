c952ae1665a349691a9b78cda14eca60335ce568
peter <dgw@yn-ce.com>
2018-03-28 13:09:49 +0800

    宇能云蟹宝盒微信redirectURL(获取用户信息)

--- weixin/build_weixinurl.php
+++ weixin/build_weixinurl.php
@@ -4,16 +4,16 @@
  * 
  */
 require_once('https_method.php');
-$redirecturl="http://ser.yn-ce.com/wx/webpage_auth.php";
+$redirecturl="https://plugs.yn-ce.com/test.php";
 $redirecturl=urlencode($redirecturl);
 
 // 不同公众号请修改appid
 if (isset($_GET['scope'])){
     // state是自定义传参
     echo 'scope=snsapi_base</br>';
-    $url="https://open.weixin.qq.com/connect/oauth2/authorize?appid=".$appinfo->t_appid."&redirect_uri=$redirecturl&response_type=code&scope=snsapi_base&state=0#wechat_redirect";
+    $url="https://open.weixin.qq.com/connect/oauth2/authorize?appid=wx56c09a46dfc4c131&redirect_uri=$redirecturl&response_type=code&scope=snsapi_base&state=0#wechat_redirect";
 }else{
     echo 'scope=snsapi_userinfo</br>';
-    $url="https://open.weixin.qq.com/connect/oauth2/authorize?appid=".$appinfo->t_appid."&redirect_uri=$redirecturl&response_type=code&scope=snsapi_userinfo&state=1#wechat_redirect";
+    $url="https://open.weixin.qq.com/connect/oauth2/authorize?appid=wx56c09a46dfc4c131&redirect_uri=$redirecturl&response_type=code&scope=snsapi_userinfo&state=1#wechat_redirect";
 }
 echo $url;
