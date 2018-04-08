f9866ffcc27fdf3bfa34a18cb0c479e57c467c68
peter <dgw@yn-ce.com>
2018-04-08 14:04:15 +0800

    Sample code improved: D test.php ?? bbc/test.php ?? ecstore/test.php

---
+++ bbc/test.php
@@ -0,0 +1,5 @@
+<?php
+// bbc php56
+require_once __DIR__.'/../b2b2c/bootstrap/autoload.php';
+require_once __DIR__.'/../b2b2c/bootstrap/start.php';
+
---
+++ ecstore/test.php
@@ -0,0 +1,12 @@
+<?php
+// ecstore php53/php56
+define('ROOT_DIR',realpath(__DIR__.'/../ecstore/'));
+require_once ROOT_DIR.'/app/base/kernel.php';
+require_once ROOT_DIR.'/app/base/autoload.php';
+require_once ROOT_DIR.'/config/config.php';
+require_once ROOT_DIR.'/app/base/ego/ego.php';
+require_once ROOT_DIR.'/config/mapper.php';
+require_once ROOT_DIR.'/app/base/defined.php';
+error_reporting(E_ALL ^ E_NOTICE);
+
+
