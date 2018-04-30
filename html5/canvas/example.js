1f9689f9044995f5db1aa97de9b78e12d6c0d5f2
Peter <46128558@qq.com>
2018-04-30 14:28:00 +0800

    Sample code improved: M html5/canvas/canvas.md  M html5/canvas/example.js

--- html5/canvas/example.js
+++ html5/canvas/example.js
@@ -5,3 +5,12 @@ context.fillStyle="cornflowerblue";
 context.fillText('Hello Canvas',canvas.width/2-150,canvas.height/2+15);
 context.strokeStyle="blue";
 context.strokeText('Hello Canvas',canvas.width/2-150,canvas.height/2+15);
+
+// window坐标转canvas坐标
+function windowsToCanvas(canvas,x,y){
+	// 获取canvas元素的边界框(bounding box),该边界框的坐标是相对于整个窗口的
+	var bbox=canvas.getBoundingClientRect();
+	return {x:x-bbox.left*(canvas.width/bbox.width),
+			y:y-bbox.top*(canvas.height/bbox.height)
+	};
+}
