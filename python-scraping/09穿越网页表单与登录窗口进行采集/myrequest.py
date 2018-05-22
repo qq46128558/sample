a12f98069ca00aada151077b143317023b9d3b19
peter <dgw@yn-ce.com>
2018-05-22 16:39:25 +0800

    Sample code improved: M "python-scraping/09\347\251\277\350\266\212\347\275\221\351\241\265\350\241\250\345\215\225\344\270\216\347\231\273\345\275\225\347\252\227\345\217\243\350\277\233\350\241\214\351\207\207\351\233\206/myrequest.py" ?? "python-scraping/09\347\251\277\350\266\212\347\275\221\351\241\265\350\241\250\345\215\225\344\270\216\347\231\273\345\275\225\347\252\227\345\217\243\350\277\233\350\241\214\351\207\207\351\233\206/request_session.py" ?? "python-scraping/09\347\251\277\350\266\212\347\275\221\351\241\265\350\241\250\345\215\225\344\270\216\347\231\273\345\275\225\347\252\227\345\217\243\350\277\233\350\241\214\351\207\207\351\233\206/trace_cookie.py"

--- python-scraping/09穿越网页表单与登录窗口进行采集/myrequest.py
+++ python-scraping/09穿越网页表单与登录窗口进行采集/myrequest.py
@@ -1,5 +1,8 @@
 import requests
 
+# 用open函数打开的python文件对象
+# files={'uploadFile':open('./xx.jpg','rb')}
+
 params={'firstname':'Ryan','lastname':'Mitchell'}
 r=requests.post("http://pythonscraping.com/files/processing.php",data=params)
 print(r.text)
