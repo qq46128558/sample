d187476b25b32fc1a3e684bb0c19fa5124a3f4a2
peter <dgw@yn-ce.com>
2018-05-24 09:35:38 +0800

    Sample code improved: M client_redirect.py

--- python-scraping/10采集JavaScript/client_redirect.py
+++ python-scraping/10采集JavaScript/client_redirect.py
@@ -6,19 +6,24 @@ from selenium import webdriver
 import time
 from selenium.webdriver.remote.webelement import WebElement
 from selenium.common.exceptions import StaleElementReferenceException
+from selenium.common.exceptions import NoSuchElementException
 
 def waitForLoad(dirver):
-    elem=driver.find_element_by_tag_name("html")
+    # elem=driver.find_element_by_tag_name("html")
+    elem=driver.find_element_by_id("content")
     count=0
     while True:
         count+=1
-        if count>50:
+        if count>20:
             print("Timing out after 10 seconds and returning")
             return
         time.sleep(.5)
         try:
-            elem==driver.find_element_by_tag_name("html")
+            # elem==driver.find_element_by_tag_name("html")
+            elem=driver.find_element_by_id("content")
         except StaleElementReferenceException:
+            return
+        except NoSuchElementException:
             return
 
 driver=webdriver.PhantomJS(executable_path=r"D:\Software\phantomjs-2.1.1-windows\bin\phantomjs")
