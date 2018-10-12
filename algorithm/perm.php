49036867632064cd93be924130ce64970443129b
Peter <46128558@qq.com>
2018-10-12 11:38:17 +0800

    M javascript/pysearch2.html  A algorithm/maopao.php  A algorithm/perm.php  A php/sample/

---
+++ algorithm/perm.php
@@ -0,0 +1,40 @@
+<?php
+/*PHP怎么实现字符串全排列组合？*/
+
+$str='abc';
+// 字符串转换为数组
+$arr_str=str_split($str);
+// 调用perm函数
+perm($arr_str,0,strlen($str)-1);
+
+/**
+ * 定义perm函数
+ * @param $ar // 排列的字符串,传址
+ * @param $k // 初始值
+ * @param $m // 最大值
+ */
+function perm(&$ar,$k,$m){ 
+    // 初始值是否等于最大值
+    if ($k==$m){
+        // 将数组转换为字符串
+        echo join('',$ar)," ";
+    }else{
+        // 循环调用函数
+        for ($i=$k;$i<=$m;$i++){
+            // 调用swap函数 交换位置
+            swap($ar[$k],$ar[$i]);
+            // 递归调用自己
+            perm($ar,$k+1,$m);
+            // 再次调用swap函数 还原位置
+            swap($ar[$k],$ar[$i]);
+
+        }
+    }
+}
+
+function swap(&$a,&$b){
+    $c=$a;
+    $a=$b;
+    $b=$c;
+}
+
